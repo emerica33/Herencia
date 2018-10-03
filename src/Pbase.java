b9c6c7e9ba494a370bf25ff543c55941c7febd56
emerica33 <42452311+emerica33@users.noreply.github.com>
2018-10-03 13:39:01 -0300

    Herencia

--- src/Pbase.java
+++ src/Pbase.java
@@ -1,10 +1,15 @@
 
 public class Pbase {
     
-     String raza, faccion;
-     int fuerza, destreza, inteligencia, salud;
+     private String raza, faccion;
+     private int fuerza, destreza, inteligencia, salud;
         
-
+    public void atributos  (int fuerza, int destreza, int inteligencia, int salud){
+        
+    }
+    public void razaFaccion (String raza, String faccion){
+        
+    }
     public void atacar (){
         
     }
--- src/Principal.java
+++ src/Principal.java
@@ -4,22 +4,14 @@ public class Principal {
     public static void main(String[] args) {
         
            Arquero arquero1 = new Arquero ();    
-            arquero1.destreza = 15;
-            arquero1.salud = 200;
-            arquero1.fuerza = 10;
-            arquero1.inteligencia = 7;
-            arquero1.faccion = "Klaxxi";
-            arquero1.raza = "Arquero";
+            arquero1.atributos(10, 15, 8, 200);
+            arquero1.razaFaccion("Arquero", "Klaxxi");
                 arquero1.tiroImpecable();
                 arquero1.triShot();
             
-        Mago mago1 = new Mago();
-            mago1.destreza = 13;
-            mago1.salud = 165;
-            mago1.fuerza = 6;
-            mago1.inteligencia = 15;
-            mago1.faccion = "Shaohao";
-            mago1.raza = "Mago";
+           Mago mago1 = new Mago();
+            mago1.atributos(8, 12, 15, 160);
+            mago1.razaFaccion("Mago", "Shaohao");
                 mago1.regenerarMana();
                 mago1.tormentaHelada();
         
